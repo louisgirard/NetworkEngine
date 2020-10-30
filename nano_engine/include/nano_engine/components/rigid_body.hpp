8ae59ce8a3e7e94072adf3c52ca96074b8126bd1
louisgirard <louis.girard17@hotmail.com>
2020-10-30 09:57:15 +0100

    Fix physic system

--- nano_engine/include/nano_engine/components/rigid_body.hpp
+++ nano_engine/include/nano_engine/components/rigid_body.hpp
@@ -1,57 +1,77 @@
 #pragma once
 
+#include <nano_engine/components/position.hpp>
+
 #include <btBulletDynamicsCommon.h>
 
 #include <nano_engine/common.hpp>
 
-#include <nano_engine/components/position.hpp>
-
 namespace nano_engine::components
 {
+	struct RigidBodyHolder
+	{
+		btRigidBody* m_rigidBody;
+		btCollisionShape* m_collisionShape;
+		btDefaultMotionState* m_motionState;
+
+		~RigidBodyHolder()
+		{
+			delete m_rigidBody;
+			delete m_collisionShape;
+			delete m_motionState;
+		}
+	};
+
 	class RigidBody
 	{
 	public:
-		RigidBody(btCollisionShape* collisionShape, float mass, const components::Position& pos) :
-			m_collisionShape(collisionShape),
-			m_mass(mass),
-			m_localInertia(0, 0, 0)
+		RigidBody(btCollisionShape* collisionShape, float mass, const Position& pos) : m_localInertia(0, 0, 0)
 		{
+			//Create the state holder
+			m_stateHolder = std::make_shared<RigidBodyHolder>();
+
+			//Save values
+			m_stateHolder->m_collisionShape = collisionShape;
+			m_mass = mass;
+
+			//Create the bullet mass
 			btScalar btMass(mass);
 
-			// Compute Inertia
-			if (btMass != 0.f)
-				m_collisionShape->calculateLocalInertia(btMass, m_localInertia);
+			//rigidbody is dynamic if and only if mass is non zero, otherwise static
+			m_isDynamic = (mass != 0.f);
 
-			// Create Transform
+			//Compute inertia
+			if (m_isDynamic)
+			{
+				collisionShape->calculateLocalInertia(btMass, m_localInertia);
+			}
+
+			//Create the transform
 			btTransform startTransform;
 			startTransform.setIdentity();
 			startTransform.setOrigin(btVector3(pos.x, pos.y, pos.z));
-			m_motionState = new btDefaultMotionState(startTransform);
+			m_stateHolder->m_motionState = new btDefaultMotionState(startTransform);
 
-			// Create RigidBody
-			btRigidBody::btRigidBodyConstructionInfo rbInfo(btMass, m_motionState, m_collisionShape, m_localInertia);
-			m_rigidBody = new btRigidBody(rbInfo);
+			//Create the rigidbody
+			btRigidBody::btRigidBodyConstructionInfo rbInfo(btMass, m_stateHolder->m_motionState, m_stateHolder->m_collisionShape, m_localInertia);
+			m_stateHolder->m_rigidBody = new btRigidBody(rbInfo);
 		}
 
 		~RigidBody()
 		{
-			delete m_motionState;
-			delete m_collisionShape;
-			delete m_rigidBody;
+			m_stateHolder = nullptr;
 		}
 
 		bool IsInWorld() const { return m_isInWorld; }
 		void IsInWorld(bool inWorld) { m_isInWorld = inWorld; }
 
-		btRigidBody* GetRigidBody() { return m_rigidBody; }
-
+		btRigidBody* GetRigidBody() { return m_stateHolder->m_rigidBody; }
 	private:
 		bool m_isInWorld = false;
+		bool m_isDynamic = false;
 
-		btRigidBody* m_rigidBody;
-		btCollisionShape* m_collisionShape;
+		std::shared_ptr<RigidBodyHolder> m_stateHolder;
 		float m_mass;
-		btDefaultMotionState* m_motionState;
 		btVector3 m_localInertia;
 	};
 }
